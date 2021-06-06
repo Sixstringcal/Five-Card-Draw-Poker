595acfdbe21bb3015f5c8eaa796635d13d5943b5
sixstringcal <sixstringcal@gmail.com>
2021-06-06 12:50:43 -0600

    Hand gets populated

--- src/Card.java
+++ src/Card.java
@@ -1,21 +1,33 @@
 public class Card {
 
+    /**
+     * The suite the card is a part of
+     */
     public String suite;
+
+    /**
+     * The numerical value of the card
+     */
     public int value;
+
+    /**
+     * The value the user sees for the card
+     */
     public String printValue;
 
     /**
      * Initializes the Card
+     *
      * @param suite - the suite the card is in (hearts, clubs, spades, or diamonds)
      * @param value - the numerical value of the card
      */
-    public Card(String suite, int value){
+    public Card(String suite, int value) {
         this.suite = suite;
         this.value = value;
 
         // Creates a user-friendly "print value" that will be
-        // used to convey to the user what value their card is.
-        switch(value){
+        // used to convey to the user what value their card is
+        switch (value) {
             case 1:
                 printValue = "ace";
                 break;
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.LinkedList;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
@@ -6,7 +7,27 @@ public class Main {
         // Creates the deck of cards
         LinkedList<Card> deck = new LinkedList<>();
 
+        // Creates the user's hand
+        LinkedList<Card> hand = new LinkedList<>();
 
-        
+        // Iterates through all card values for each suite,
+        // populating the deck with all 52 cards
+        for (int i = 1; i < 14; i++) {
+            deck.add(new Card("hearts", i));
+            deck.add(new Card("diamonds", i));
+            deck.add(new Card("spades", i));
+            deck.add(new Card("clubs", i));
+        }
+
+        // Creates the random number generator
+        Random rng = new Random();
+
+        // Takes the cards from the deck and places them in
+        // the user's hand
+        for (int i = 0; i < 5; i++) {
+            hand.add(deck.remove(rng.nextInt(deck.size())));
+        }
+
+
     }
 }
