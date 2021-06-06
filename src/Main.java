1d4d0dbf2e5207c8ee006d1ab8c9156e0f00c4ee
sixstringcal <sixstringcal@gmail.com>
2021-06-06 13:35:12 -0600

    Swapping cards works.  Currently, if it's in an inproper format, there's an exception thrown and the program ends. Will fix.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,6 +12,12 @@ public class Main {
         // Creates the user's hand
         LinkedList<Card> hand = new LinkedList<>();
 
+        // Scanner to read the input from the user
+        Scanner scanner = new Scanner(System.in);
+
+        // Creates the random number generator
+        Random rng = new Random();
+
         // Iterates through all card values for each suite,
         // populating the deck with all 52 cards
         for (int i = 1; i < 14; i++) {
@@ -19,15 +27,11 @@ public class Main {
             deck.add(new Card("clubs", i));
         }
 
-        // Creates the random number generator
-        Random rng = new Random();
-
         // Takes the cards from the deck and places them in
         // the user's hand
         for (int i = 0; i < 5; i++) {
             hand.add(deck.remove(rng.nextInt(deck.size())));
         }
-
 
         // Displays to the user the hand
         System.out.println("Your hand is:");
@@ -35,11 +39,43 @@ public class Main {
             System.out.println("\t (" + (i + 1) + ") " + hand.get(i).printValue + " of " + hand.get(i).suite);
         }
 
-        //Explains to the user how to play.
+        //Explains to the user how to play
         System.out.println("Select the cards you would like to discard (example: \"1, 2, 4\").");
         System.out.println("Type \"hold\" to hold");
 
+        // Reads and deciphers the user's input
+        String input = scanner.nextLine();
+        if (!input.toLowerCase().equals("hold")) {
+            try {
+                String[] split = input.split(",");
+                // Hash set used to deal with duplicates.
+                HashSet<Integer> toSwap = new HashSet<>();
 
+                // Gets the index of each card to be swapped
+                for (int i = 0; i < split.length; i++) {
+                    toSwap.add(Integer.parseInt(split[i].trim()) - 1);
+                }
 
+                // Return the cards from the hand to the deck
+                for (int temp : toSwap) {
+                    deck.add(hand.get(temp));
+                }
+
+                // Replaces the cards in the hand
+                for (int temp : toSwap) {
+                    hand.set(temp, deck.remove(rng.nextInt(deck.size())));
+                }
+
+            } catch (Exception e) {
+                System.out.println("Invalid input");
+            }
+
+        }
+
+        // Displays to the user the new hand
+        System.out.println("Your new hand is:");
+        for (int i = 0; i < hand.size(); i++) {
+            System.out.println("\t" + hand.get(i).printValue + " of " + hand.get(i).suite);
+        }
     }
 }
